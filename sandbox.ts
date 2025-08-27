b8f7381825ae1a588217c48f33ac899b4d0245cd
Fernando <fernandop@pmgintelligence.com>
2025-08-27 11:07:33 -0400

    lesson 3

--- sandbox.ts
+++ sandbox.ts
@@ -1,7 +1,27 @@
-const character = "Fernando";
+let character = "Mr. Borges";
+let age = 44;
+let isBlackBelt = false;
 
-console.log(`Hello, ${character}!`);
+character = "Fernando";
 
-const inputs = document.querySelectorAll("input");
+age = 43;
+isBlackBelt = true;
 
-console.log(inputs);
+console.log(character);
+console.log(age);
+console.log(isBlackBelt);
+
+const circ = (diameter: number) => {
+	return diameter * Math.PI;
+};
+
+console.log(circ(7.5));
+// console.log(`Hello, ${character}!`);
+
+// const inputs = document.querySelectorAll("input");
+
+// console.log(inputs);
+
+// inputs.forEach(input => {
+//     console.log(input);
+// });
