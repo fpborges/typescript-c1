59689bd5b96677fea3c28ab1c2cac17fff89c8bc
Fernando <fernandop@pmgintelligence.com>
2025-09-02 17:16:47 -0400

    update code

--- public/app.js
+++ public/app.js
@@ -1,3 +1,26 @@
+//classes
+class Invoice {
+    client;
+    details;
+    amount;
+    constructor(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    format() {
+        return `${this.client} owes $${this.amount} for ${this.details}`;
+    }
+}
+const invOne = new Invoice("mario", "work on the mario website", 250);
+const invTwo = new Invoice("luigi", "work on the luigi website", 300);
+//only allow Invoice objects in the array
+let invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+invoices.forEach((inv) => {
+    console.log(inv.client, inv.amount, inv.format());
+});
 const anchor = document.querySelector("a");
 console.log(anchor?.href);
 // const form = document.querySelector("form")!;
