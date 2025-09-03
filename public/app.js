738db1633407a83e8ac7a821e81a44c6179dd3a0
Fernando <fernandop@pmgintelligence.com>
2025-09-03 10:45:30 -0400

    lesson 14 done

--- public/app.js
+++ public/app.js
@@ -1,17 +1,4 @@
-//classes
-class Invoice {
-    client;
-    details;
-    amount;
-    constructor(c, d, a) {
-        this.client = c;
-        this.details = d;
-        this.amount = a;
-    }
-    format() {
-        return `${this.client} owes $${this.amount} for ${this.details}`;
-    }
-}
+import { Invoice } from "./classes/Invoice.js";
 const invOne = new Invoice("mario", "work on the mario website", 250);
 const invTwo = new Invoice("luigi", "work on the luigi website", 300);
 //only allow Invoice objects in the array
@@ -22,7 +9,7 @@ invoices.forEach((inv) => {
     console.log(inv.client, inv.amount, inv.format());
 });
 const anchor = document.querySelector("a");
-console.log(anchor?.href);
+console.log(anchor === null || anchor === void 0 ? void 0 : anchor.href);
 // const form = document.querySelector("form")!;
 const form = document.querySelector(".new-item-form");
 console.log(form.children);
